4d890811c0249d690609585def373bc7a54adee5
Paul Seguinard <paul.seguinard@epitech.eu>
2020-10-08 15:02:19 +0200

    [WIP] movements, drop and throw chicken

---
+++ Assets/src/Scripts/GrabChicken.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabChicken : MonoBehaviour
+{
+
+    [SerializeField] private GameObject chickenGrabbed;
+
+    private GameObject chickenCanGrab;
+
+    private bool canGrab = false;
+
+    [SerializeField] private float chickenVelocity = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) &&  chickenGrabbed != null)
+        {
+            DropChicken();
+        }
+        else if (Input.GetMouseButtonDown(0) && canGrab == true && chickenGrabbed == null)
+        {
+            GrabTheChicken();
+        }
+        else if (Input.GetMouseButtonDown(1) && chickenGrabbed != null)
+        {
+            ThrowChicken();
+        }
+    }
+
+    private void ThrowChicken()
+    {
+        GameObject closestGuard = FindClosestEnemy();
+
+        Vector3 normalizeDirection = (closestGuard.transform.position - transform.position).normalized;
+
+
+        //chickenGrabbed.tranform.position += normalizeDirection * chickenVelocity * Time.deltaTime;
+
+        chickenGrabbed.GetComponent<Rigidbody>().velocity = normalizeDirection * chickenVelocity;
+
+
+        chickenGrabbed.transform.parent = null;
+
+        chickenGrabbed = null;
+    }
+
+
+    private void DropChicken()
+    {
+        chickenGrabbed.transform.parent = null;
+
+        chickenGrabbed = null;
+    }
+
+    // graab the chicken and make it children of player object
+    private void GrabTheChicken()
+    {
+        if (chickenCanGrab == null || canGrab == false)
+            return;
+
+        chickenCanGrab.transform.SetParent(transform.transform);
+
+        chickenCanGrab.transform.localPosition = new Vector3(0, 1.2f, 0);
+
+        chickenGrabbed = chickenCanGrab;
+        chickenCanGrab = null;
+        canGrab = true;
+    }
+
+    // get a chicken close to you
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.tag != "Chicken")
+            return;
+
+        chickenCanGrab = other.transform.gameObject;
+
+
+        canGrab = true;
+    }
+
+    // clear chicken close to you
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag != "Chicken")
+            return;
+
+        chickenCanGrab = null;
+        canGrab = false;
+    }
+
+
+    public GameObject FindClosestEnemy()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Guard");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+}
